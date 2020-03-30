363ae1d2a99d2a9ba15965e96187a9a98e111cf5
kdw9502 <kdw9502@gmail.com>
2020-03-30 18:42:16 +0900

    temperature-api

---
+++ temperature-api.py
@@ -0,0 +1,39 @@
+import pymysql
+import requests
+import xml.etree.ElementTree as ET
+import datetime
+
+url = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1159068000"
+
+text = requests.get(url).text
+root = ET.fromstring(text)
+
+description = root.find('channel').find('item').find('description')
+body = description.find('body')
+header = description.find('header')
+db = pymysql.connect(
+    user='kdw9502',
+    passwd='rkdehddnr1',
+    host='18.232.93.89',
+    db='kdw9502_db',
+    charset='utf8'
+)
+cursor = db.cursor()
+
+ 
+time_string = header.find("tm").text
+year = int(time_string[0:4])
+month = int(time_string[4:6])
+day = int(time_string[6:8])
+hour = int(time_string[8:10])
+minute = int(time_string[10:12])
+
+data = body.find('data')
+
+base_datetime = datetime.datetime(year, month, day, hour, minute).strftime('%Y-%m-%d %H:%M:%S')
+
+temperature = float(data.find("temp").text)
+
+query = f"INSERT INTO temperature(base_time, hour, temp) VALUES ('{base_datetime}', {0},{temperature});"
+cursor.execute(query)
+db.commit()
